5bd8503b0f7523a5b3729d18d393dffcfbca31af
vedx07 <code.vedx@gmail.com>
2026-05-25 11:55:22 +0530

    Deploy Ready

--- server/index.js
+++ server/index.js
@@ -13,6 +13,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('API Running');
+});
+
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/upload', uploadRoutes);
